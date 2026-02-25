3a99bb6c2e52076691391a638aaea5f7554045f0
Yurii Surniak <surniakyura@gmail.com>
2026-02-26 01:07:10 +0200

    refactor: clean slate, new 4-layer architecture for backend

--- vidnova_backend/Data/AppDbContext.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace Vidnova.Presentation.Api.Data;
-
-public class AppDbContext: DbContext
-{
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-}
---
+++ vidnova_backend/Vidnova.Infrastructure/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Vidnova.Infrastructure;
+
+public class Class1
+{
+}
